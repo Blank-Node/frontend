19ebe114bfd9cd71fd1d7e56a24e0d69427907a3
Rui de Klerk <rvzk.mota@gmail.com>
2024-02-01 17:40:09 +0000

    add title

--- src/Components/Branding/Branding.tsx
+++ src/Components/Branding/Branding.tsx
@@ -1,14 +1,22 @@
 import { FC } from 'react'
-import { Flex, Divider, Space } from 'antd'
+import { Flex, Divider, Space, Typography } from 'antd'
 import { MailOutlined, LinkedinOutlined } from '@ant-design/icons'
-
 import AnimatedLines from '../Viz/AnimatedLines'
+
+const { Text, Title } = Typography
 
 const Branding: FC<any> = ({ children }) => {
   return (
     <>
-      <Flex className='full' style={{ marginBottom: '1em' }}>
+      <Flex className='full' gap={12} style={{ marginBottom: '1em', alignItems: 'center'}}>
         <img src={'./img/BN__icon_300.png'} className="logo-small" alt="Blank Node" />
+        <Title style={{
+          fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+          color: 'lightGrey',
+          fontWeight: 400,
+          margin: 0,
+          marginBlock: 0
+        }}>{'Blank Node'}</Title>
       </Flex>
       {/* {children} */}
       <div style={{ height: window.innerHeight - 160 }}>
@@ -18,7 +26,12 @@ const Branding: FC<any> = ({ children }) => {
       <div style={{ bottom: 0 }}>
         <Divider style={{ border: '1px solid grey' }}/>
         <Flex justify='space-between' style={{ color: 'grey' }}>
-          <text>© 2023 {<a id='link-blanknode' href='https://blanknode.dev/' target='_blank' rel="noreferrer">Blank Node</a>}. All rights reserved</text>
+          <Text style={{
+            fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+            color: 'lightGrey',
+            fontWeight: 400,
+            fontSize: '1em',
+          }}>© 2023-2024 {<a id='link-blanknode' href='https://blanknode.dev/' target='_blank' rel="noreferrer">Blank Node</a>}. All rights reserved</Text>
 
           <Space size={48}>
             <Space direction='horizontal' size={8}>
